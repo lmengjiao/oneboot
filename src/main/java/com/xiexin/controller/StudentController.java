a268c8db1104b9d182b0fe6cb573a36eca1590b2
lin <2712802058@qq.com>
2021-10-09 12:36:36 +0800

    分页查询

--- src/main/java/com/xiexin/controller/StudentController.java
+++ src/main/java/com/xiexin/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.xiexin.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.xiexin.bean.Student;
+import com.xiexin.bean.StudentExample;
 import com.xiexin.respcode.Result;
 import com.xiexin.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,9 +27,29 @@ public class StudentController {
         //return new Result(studentService.selectByExample(null));
     }
 
-    //分页查询（带参数）
+    //分页查询
     @RequestMapping("/selectPageAll")
-    public Result selectPageAll(){
-        return null;
+    public Result selectPageAll(Student student,@RequestParam(value="page",defaultValue = "1",required = true) Integer page,
+                                @RequestParam(value="limit",defaultValue = "5",required = true) Integer pageSize) {
+        System.out.println("student = " + student);
+        StudentExample example = new StudentExample();
+        StudentExample.Criteria criteria = example.createCriteria();
+        //使用pageHer分页
+        PageHelper.startPage(page, pageSize);
+        if(null!=student.getStudentSex()&&!"".equals(student.getStudentSex())){
+            criteria.andStudentSexEqualTo(student.getStudentSex());
+        }
+        if(null!=student.getStudentName()&&!"".equals(student.getStudentName())){
+            criteria.andStudentNameEqualTo(student.getStudentName());
+        }
+        List<Student> students = studentService.selectByExample(example);
+        PageInfo pageInfo = new PageInfo(students);
+        Result result = new Result(pageInfo);
+        return result;
     }
+
+    //搜索框 学号 姓名 性别 身份证号
+//    public Result selectBysousuo(){
+//        studentService.selectByExample(studentId,)
+//    }
 }
